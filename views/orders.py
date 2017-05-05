79375ce175a9f13c7f0c3f2d476384a83a7ce9f0
Daniele Ding <dinghino@gmail.com>
2017-05-05 14:34:06 +0200

    refactor var names in views/orders

--- views/orders.py
+++ views/orders.py
@@ -32,18 +32,18 @@ class OrdersHandler(Resource):
             return errors, BAD_REQUEST
 
         # Extract data to create the new order
-        res_items = res['data']['relationships']['items']['data']
-        res_address = res['data']['relationships']['delivery_address']['data']
+        req_items = res['data']['relationships']['items']['data']
+        req_address = res['data']['relationships']['delivery_address']['data']
 
         # Check that the items exist
-        item_ids = [res_item['item_id'] for res_item in res_items]
+        item_ids = [req_item['item_id'] for req_item in req_items]
         items = Item.select().where(Item.item_id << item_ids)
-        if items.count() != len(res_items):
+        if items.count() != len(req_items):
             abort(BAD_REQUEST)
 
         # Check that the address exist
         try:
-            address = Address.get(Address.address_id == res_address['id'])
+            address = Address.get(Address.address_id == req_address['id'])
         except Address.DoesNotExist:
             abort(BAD_REQUEST)
 
@@ -55,11 +55,11 @@ class OrdersHandler(Resource):
                 )
 
                 for item in items:
-                    for res_item in res_items:
+                    for req_item in req_items:
                         # if names match add item and quantity, once per
-                        # res_item
-                        if str(item.item_id) == res_item['id']:
-                            order.add_item(item, res_item['quantity'])
+                        # req_item
+                        if str(item.item_id) == req_item['id']:
+                            order.add_item(item, req_item['quantity'])
                             break
             except InsufficientAvailabilityException:
                 txn.rollback()
@@ -98,21 +98,18 @@ class OrderHandler(Resource):
         if not isValid:
             return errors, BAD_REQUEST
 
-        res_items = res['data']['relationships']['items']['data']
-        res_addr = res['data']['relationships']['delivery_address']['data']
+        req_items = res['data']['relationships']['items']['data']
+        req_address = res['data']['relationships']['delivery_address']['data']
 
         try:
             order = Order.get(order_id=order_id)
-            items_ids = [e['item_id'] for e in res_items]
-            address = Address.get(Address.address_id == res_addr['id'])
-            items_ids = [e['id'] for e in res_items]
-
+            address = Address.get(Address.address_id == req_address['id'])
+            items_ids = [e['id'] for e in req_items]
             items = list(Item.select().where(Item.item_id << items_ids))
             if len(items) != len(items_ids):
                 return None, BAD_REQUEST
         except (Address.DoesNotExist, Order.DoesNotExist):
             return None, NOT_FOUND
-
         # get the user from the flask.g global object registered inside the
         # auth.py::verify() function, called by @auth.login_required decorator
         # and match it against the found user.
@@ -122,28 +119,35 @@ class OrderHandler(Resource):
                     UNAUTHORIZED)
 
         with database.transaction() as txn:
-            try:
-                # Clear the order of all items before adding the new items
-                # that came with the PATCH request
-                order.empty_order()
+            # Clear the order of all items before adding the new items
+            # that came with the PATCH request
+            order.empty_order()
 
-                for item in items:
-                    for res_item in res_items:
-                        # if id match add item and quantity, once per
-                        # res_item
-                        if str(item.item_id) == res_item['id']:
-                            order.add_item(item, res_item['quantity'])
-                            break
-            except InsufficientAvailabilityException:
-                txn.rollback()
-                return None, BAD_REQUEST
-            except KeyError:
-                # FIXME: Prevent future KeyError when adding items. See post method
-                # for further info.
-                msg = {
-                    'message': 'Item {} missing quantity attribute'.format(item.item_id)
-                }
-                return msg, BAD_REQUEST
+            for req_item in req_items:
+                item = next(i for i in items if str(
+                    i.item_id) == req_item['id'])
+                try:
+                    # Clear the order of all items before adding the new items
+                    # that came with the PATCH request
+                    order.empty_order()
+
+                    for item in items:
+                        for req_item in req_items:
+                            # if id match add item and quantity, once per
+                            # req_item
+                            if str(item.item_id) == req_item['id']:
+                                order.add_item(item, req_item['quantity'])
+                                break
+                except InsufficientAvailabilityException:
+                    txn.rollback()
+                    return None, BAD_REQUEST
+                except KeyError:
+                    # FIXME: Prevent future KeyError when adding items. See post method
+                    # for further info.
+                    msg = {
+                        'message': 'Item {} missing quantity attribute'.format(item.item_id)
+                    }
+                    return msg, BAD_REQUEST
 
         order.delivery_address = address
         order.save()
